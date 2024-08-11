4fa6dd67104951c2d2929cc75c8a989c5dddda55
Arestalia <ztrxds.2290@gmail.com>
2024-08-11 21:26:45 +0700

    Update index.php

--- Attendance/index.php
+++ Attendance/index.php
@@ -8,11 +8,10 @@
 <body>
     <h1>Selamat Datang di Sistem Absensi RFID</h1>
     <ul>
-        <li><a href="input_siswa.html">Input Data Siswa</a></li>
-        <li><a href="cari_siswa.html">Cari Data Siswa</a></li>
+        <li><a href="input_siswa.php">Input Data Siswa</a></li>
+        <li><a href="login.php">login</a></li>
         <li><a href="absensi.php">Absensi Siswa</a></li>
-        <li><a href="dashboard.php">Dashboard Absensi</a></li>
     </ul>
 </body>
 
-</html> 
+</html>
