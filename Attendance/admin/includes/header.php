6806d709b22e134b3cc0c51a07f3fed2e35186ab
Arestalia <ztrxds.2290@gmail.com>
2024-08-20 03:22:11 +0700

    Create header.php

---
+++ Attendance/admin/includes/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin</title>
+    <link rel="stylesheet" href="../assets/css/index.css">
+</head>
+
+<body>
+    <header>
+        <h1>Admin Absensi</h1>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="add.php">Add Student</a>
+            <a href="list_siswa.php">List Students</a>
+            <a href="list.php">List Absensi</a>
+            <a href="../public/absensi.php">Absensi</a>
+            <a href="logout.php">Logout</a>
+
+        </nav>
+    </header>
+    <main>
